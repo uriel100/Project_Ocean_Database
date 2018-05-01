342c799ad091325bf99fc1cf8a05c59841a6e629
Christiana Huiskens <38085596+xtianah@users.noreply.github.com>
2018-05-01 14:28:07 -0700

    Create BST.h

---
+++ BST.h
@@ -0,0 +1,28 @@
+# include <iostream>
+# include <cstdlib>
+# include <cctype>
+# include <string>
+# include <vector>
+# include <iterator>
+# include <algorithm>
+
+using namespace std;
+
+class Node{
+    Node* left = NULL;
+    Node* right = NULL;
+    Node* parent = NULL;
+    Node* root = NULL;
+    char data;
+    public:
+    Node(){}
+    void setLeft(Node* l){left = l;};
+    void setRight(Node* r){right = r;};
+    void setRoot(Node* rt){root = rt;}
+    void setParent(Node* p){parent = p;};
+    void setData(char sad){data = sad;};
+    Node* getRoot(){return root;};
+    Node* getLeft(){return left;};
+    Node* getRight(){return right;};
+    char getData(){return data;};
+};
