5fa2a12a75caa6278f038f79b8e3cf4325ee395a
maplemaplemaple <38085675+maplemaplemaple@users.noreply.github.com>
2018-05-03 14:01:21 -0700

    Update Heap.h

--- Heap.h
+++ Heap.h
@@ -7,3 +7,22 @@
 # include <algorithm>
 
 using namespace std;
+
+Class LL {
+private:
+  int animal = 0;
+  string address;
+  string url;
+  string name;
+ 
+  
+public:
+  LL( int new_animal = 0, string new_name = " ", string new_address = " ", string new_url = " ", LL *new_prev = nullptr, LL *new_nect= nullptr)
+    
+ animal = new_animal;
+ name = new_name;
+ address = new_address;
+ url = new_url;
+
+
+};
