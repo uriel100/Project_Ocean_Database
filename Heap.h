86cdcf24d006ff8336a47822a6d4db56d455360c
maplemaplemaple <38085675+maplemaplemaple@users.noreply.github.com>
2018-05-03 15:06:21 -0700

    Update Heap.h

--- Heap.h
+++ Heap.h
@@ -28,10 +28,10 @@ public:
   LL( int new_animal = 0, string new_name = " ", string new_address = " ", string new_url = " ", LL *new_prev = nullptr, LL *new_next= nullptr)
   {
     animal = new_animal;
-  if ( animal < 1 ) {
+  if (new_animal < 1 ) {
     animal = 1; 
   }
-   else  if (animal > 8) {
+   else  if (new_animal > 8) {
    animal = 8;
    } // in main i need to cout error messages. this why we are clamp
  
@@ -45,8 +45,33 @@ public:
   int get_new_animal() {
     return animal;
   }
+    
   void set_animal( int new_animal) {
-  animal = new_animal;
+      if (new_animal < 1 ) {
+           animal = 1; 
+           }
+   else  if (new_animal > 8) {
+        animal = 8;
+        }
+       else animal = new_animal;
   }
-
+    
+  string get_name() {
+  return name;
+  }
+void set_name(string new_name) {
+    name = new_name;
+}
+string get_address(){
+return address;
+}
+void set_address(string new_address) {
+address = new_address;
+}
+string get_url(){
+return url;
+}    
+void set_url( string new_url) {
+    url = new_url;
+}
 };
